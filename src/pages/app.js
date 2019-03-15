71a538e5868c959788aae42cc9159613d835dda9
Binyomin Greenes <bygreenes@gmail.com>
2019-03-15 14:41:45 -0400

    use react-elm-components

--- src/pages/app.js
+++ src/pages/app.js
@@ -3,21 +3,16 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-
-import {Elm} from '../elm/Main.elm';
+import Counter from '../elm/Main.elm';
+import Elm from 'react-elm-components'
 
 
 
 class SecondPage extends React.Component {
-  componentDidMount() {
-    var app = Elm.Main.init({
-      node: document.getElementById('main')
-    });
-  }
   render() {
     return (<Layout>
     <SEO title="page 2" keywords={[`gatsby`, `application`, `react`]} />
-    <div id="main">Elm app loading...</div>
+    <Elm src={Counter.Elm.Main}/>
     <Link to="/">Go to home</Link>
   </Layout>)
   }
